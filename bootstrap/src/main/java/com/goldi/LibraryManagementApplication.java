c58e079f61cf8061d68a9754670bb08b6db40f87
goldipandey <pandey.goldi131@gmail.com>
2020-11-12 09:24:49 +0530

    feat(bootstrap): added main bootstrap class

---
+++ bootstrap/src/main/java/com/goldi/LibraryManagementApplication.java
@@ -0,0 +1,6 @@
+package com.goldi;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) { }
+}
---
+++ bootstrap/src/main/java/com/goldi/config/LibraryConfig.java
@@ -0,0 +1,4 @@
+package com.goldi.config;
+
+public class LibraryConfig {
+}
